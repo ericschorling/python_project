c661eb08763ea106203805e0c315a806b0219765
Harmony Trevena <harmonytrevena@gmail.com>
2020-08-07 13:52:08 -0400

    Updated graphics and game boards. Moved Boss computer location.

--- Characters_and_Gameboard/splash_screen.py
+++ Characters_and_Gameboard/splash_screen.py
@@ -1,18 +1,18 @@
-import pygame
-import math
+# import pygame
+# import math
 
-#Intialize Pygame
-pygame.init()
+# #Intialize Pygame
+# pygame.init()
 
-#Create the screen W,H
-screen = pygame.display.set_mode((800,600))
-# This was relocated below: screenImg = pygame.image.load("Game_Board.png").convert()
+# #Create the screen W,H
+# screen = pygame.display.set_mode((800,600))
+# # This was relocated below: screenImg = pygame.image.load("Game_Board.png").convert()
     
 
-#Title and Icon 
-pygame.display.set_caption("Game Name")
+# #Title and Icon 
+# pygame.display.set_caption("Game Name")
 
-# Define game intro buttons
+# # Define game intro buttons
 # def button(x,y,w,h,load_game=None):
 #     mouse = pygame.mouse.get_pos()
 #     click = pygame.mouse.get_pressed()
@@ -22,114 +22,114 @@ pygame.display.set_caption("Game Name")
 #             if load_game == "play":
 #                 # running() - add game play function
 
-#Created a game intro
-def game_intro():
-    intro = True
-    while intro:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+# #Created a game intro
+# def game_intro():
+#     intro = True
+#     while intro:
+#         for event in pygame.event.get():
+#             print(event)
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 quit()
         
-        introImg = pygame.image.load("Game_Opening_Screen.png").convert()
-        screen.blit(introImg,(0,0))
-        # button(357,480,122,33,"play")
+#         introImg = pygame.image.load("Game_Opening_Screen.png").convert()
+#         screen.blit(introImg,(0,0))
+#         # button(357,480,122,33,"play")
         
-        pygame.display.update()
+#         pygame.display.update()
 
 
-#Player
-playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
-playerX = 175
-playerY = 345
-playerX_change = 0
-playerY_change = 0
+# #Player
+# playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
+# playerX = 175
+# playerY = 345
+# playerX_change = 0
+# playerY_change = 0
 
-def player(x,y):
-    #Drawing image to screen
-    screen.blit(playerImg,(x,y))
+# def player(x,y):
+#     #Drawing image to screen
+#     screen.blit(playerImg,(x,y))
 
-#computer
-computerImg = pygame.image.load('laptop.png')
+# #computer
+# computerImg = pygame.image.load('laptop.png')
 
-compx = [72,400,80,650]
-compy = [113,400,368,400]
-def computer ():
-    screen.blit(computerImg,(compx[0],compy[0]))
-    screen.blit(computerImg,(compx[1],compy[1]))
-    screen.blit(computerImg,(compx[2],compy[2]))
-    screen.blit(computerImg,(compx[3],compy[3]))
+# compx = [72,400,80,650]
+# compy = [113,400,368,400]
+# def computer ():
+#     screen.blit(computerImg,(compx[0],compy[0]))
+#     screen.blit(computerImg,(compx[1],compy[1]))
+#     screen.blit(computerImg,(compx[2],compy[2]))
+#     screen.blit(computerImg,(compx[3],compy[3]))
     
-#Game Loop
-# def game_loop():
-running = True
-while running:
+# #Game Loop
+# # def game_loop():
+# running = True
+# while running:
 
-    screenImg = pygame.image.load("Game_Board.png").convert()
-    screen.blit(screenImg,(0,0))
+#     screenImg = pygame.image.load("Game_Board.png").convert()
+#     screen.blit(screenImg,(0,0))
 
-    #Gets events from inside the Pygame screen
-    for event in pygame.event.get():
-        #Checking if x quit button is pressed
-        if event.type == pygame.QUIT:
-            #Condition to exit the while loop
-            running = False 
-        #if a keystroke is pressed check whether right or left 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -4
-                playerImg = pygame.image.load("Sean_Walking_Left.png").convert_alpha()
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 4
-                playerImg = pygame.image.load("Sean_Walking_Right.png").convert_alpha()
-            if event.key == pygame.K_UP:
-                playerY_change = -4
-                playerImg = pygame.image.load("Sean_Float_Up.png").convert_alpha()
-            if event.key == pygame.K_DOWN:
-                playerY_change = 4
+#     #Gets events from inside the Pygame screen
+#     for event in pygame.event.get():
+#         #Checking if x quit button is pressed
+#         if event.type == pygame.QUIT:
+#             #Condition to exit the while loop
+#             running = False 
+#         #if a keystroke is pressed check whether right or left 
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_LEFT:
+#                 playerX_change = -4
+#                 playerImg = pygame.image.load("Sean_Walking_Left.png").convert_alpha()
+#             if event.key == pygame.K_RIGHT:
+#                 playerX_change = 4
+#                 playerImg = pygame.image.load("Sean_Walking_Right.png").convert_alpha()
+#             if event.key == pygame.K_UP:
+#                 playerY_change = -4
+#                 playerImg = pygame.image.load("Sean_Float_Up.png").convert_alpha()
+#             if event.key == pygame.K_DOWN:
+#                 playerY_change = 4
 
-        if event.type ==  pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                playerImg = pygame.image.load("Sean_Walking_Right_Alt.png").convert_alpha()
-                playerX_change = 0
-                playerY_change = 0
+#         if event.type ==  pygame.KEYUP:
+#             if event.key == pygame.K_RIGHT:
+#                 playerImg = pygame.image.load("Sean_Walking_Right_Alt.png").convert_alpha()
+#                 playerX_change = 0
+#                 playerY_change = 0
 
-            if event.key == pygame.K_LEFT:
-                playerImg = pygame.image.load("Sean_Walking_Left_Alt.png").convert_alpha()
-                playerX_change = 0
-                playerY_change = 0
+#             if event.key == pygame.K_LEFT:
+#                 playerImg = pygame.image.load("Sean_Walking_Left_Alt.png").convert_alpha()
+#                 playerX_change = 0
+#                 playerY_change = 0
 
-            if event.key == pygame.K_UP:
-                playerImg = pygame.image.load("Sean_Float_Up_Alt.png").convert_alpha()
-                playerX_change = 0
-                playerY_change = 0
+#             if event.key == pygame.K_UP:
+#                 playerImg = pygame.image.load("Sean_Float_Up_Alt.png").convert_alpha()
+#                 playerX_change = 0
+#                 playerY_change = 0
 
-            if event.key == pygame.K_DOWN:
-                playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
-                playerX_change = 0
-                playerY_change = 0
+#             if event.key == pygame.K_DOWN:
+#                 playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
+#                 playerX_change = 0
+#                 playerY_change = 0
 
-    # Drawing the player on the screen with a boundary
-    playerX += playerX_change
-    playerY += playerY_change
-    if playerX <= 0:
-        playerX = 0 
-    elif playerX >= 625: 
-        playerX = 625
-    if playerY <= 0:
-        playerY = 0 
-    elif playerY >= 345:
-        playerY = 345
+#     # Drawing the player on the screen with a boundary
+#     playerX += playerX_change
+#     playerY += playerY_change
+#     if playerX <= 0:
+#         playerX = 0 
+#     elif playerX >= 625: 
+#         playerX = 625
+#     if playerY <= 0:
+#         playerY = 0 
+#     elif playerY >= 345:
+#         playerY = 345
 
-    # pygame.display.update() 
+#     # pygame.display.update() 
     
-    #fuction calls game intro
-    game_intro()
-    #Calls game loop
-    # Need to call game_loop function
-    #fuction to call computer
-    computer()
-    # #function to display player
-    player(playerX,playerY)
-    pygame.display.update() 
+#     #fuction calls game intro
+#     game_intro()
+#     #Calls game loop
+#     # Need to call game_loop function
+#     #fuction to call computer
+#     computer()
+#     # #function to display player
+#     player(playerX,playerY)
+#     pygame.display.update() 
--- player_animation.py
+++ player_animation.py
@@ -22,7 +22,7 @@ narration_box = Message_Box(20, 400, 50 )
 #Background 
 # background = pygame.image.load('bg.jpg')
 #Title and Icon 
-pygame.display.set_caption("Game Name")
+pygame.display.set_caption("Sean vs AI!")
 
 #Player
 playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
@@ -36,6 +36,7 @@ playerY_change = 0
 BLACK_BACKGROUND = (0,0,0)
 #computer change to have a number for each one
 computerImg = pygame.image.load('laptop.png')
+bossImg = pygame.image.load('Final_Computer.png')
 #computer flag
 c1 = 0
 c2 = 0
@@ -44,7 +45,7 @@ c4 = 0
 c5 = 0
 
 compx = [72,80,400,650,400]
-compy = [113,368,368,368,200]
+compy = [113,368,368,368,108]
 def computer (end_game):
     
     screen.blit(computerImg,(compx[0],compy[0]))
@@ -52,7 +53,7 @@ def computer (end_game):
     screen.blit(computerImg,(compx[2],compy[2]))
     screen.blit(computerImg,(compx[3],compy[3]))
     #add endgame computer image only when endgame is triggered.
-    if end_game: screen.blit(computerImg,(compx[4],compy[4]))
+    if end_game: screen.blit(bossImg,(compx[4],compy[4]))
 
 def player(x,y):
     #Drawing image to screen
@@ -113,7 +114,7 @@ while running:
                 playerImg = pygame.image.load("Sean_Float_Up.png").convert_alpha()
             if event.key == pygame.K_DOWN:
                 playerY_change = 4
-                playerImg = pygame.image.load("Sean_Front.png").convert_alpha()
+                playerImg = pygame.image.load("Sean_Down.png").convert_alpha()
 
         if event.type ==  pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
@@ -127,7 +128,7 @@ while running:
                 playerY_change = 0
 
             if event.key == pygame.K_UP:
-                playerImg = pygame.image.load("Sean_Float_Up_Alt.png").convert_alpha()
+                # playerImg = pygame.image.load("Sean_Float_Up_Alt.png").convert_alpha()
                 playerX_change = 0
                 playerY_change = 0
 
